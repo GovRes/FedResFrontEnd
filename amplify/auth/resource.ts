e8aa8e8429c322e5972547f0d4ab127ed7f53776
Aili <aili.k.huber@gmail.com>
2025-01-15 16:32:28 -0500

    resetting users

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,4 +1,5 @@
-// import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth } from '@aws-amplify/backend';
+export const auth = {}
 
 // export const auth = defineAuth({
 //   loginWith: {
--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,5 +1,5 @@
 import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
-
+export const data = {}
 /*== STEP 1 ===============================================================
 The section below creates a Todo database table with a "content" field. Try
 adding a new "isDone" field as a boolean. The authorization rule below
