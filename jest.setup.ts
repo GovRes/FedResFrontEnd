941c5bdf6bf3b012f716cefc531362d9df23f165
Aili <aili.k.huber@gmail.com>
2025-07-10 11:54:19 -0400

    dependency hell

--- jest.setup.ts
+++
@@ -1,8 +0,0 @@
-import '@testing-library/jest-dom';
-
-// Mock ResizeObserver which is not available in jsdom
-global.ResizeObserver = jest.fn().mockImplementation(() => ({
-    observe: jest.fn(),
-    unobserve: jest.fn(),
-    disconnect: jest.fn(),
-})); 
